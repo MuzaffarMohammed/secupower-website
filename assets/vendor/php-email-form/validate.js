096c3f3dc795f7372ca79e58dd862b733eb8c7ab
MuzaffarMohammed <muzaffarmohammed43@gmail.com>
2021-03-06 00:39:54 +0530

    Contact form changes

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,9 +1,14 @@
-jQuery(document).ready(function($) {
+/**
+* PHP Email Form Validation - v2.3
+* URL: https://bootstrapmade.com/php-email-form/
+* Author: BootstrapMade.com
+*/
+!(function($) {
   "use strict";
 
-  //Contact
-  $('form.php-email-form').submit(function() {
-   
+  $('form.php-email-form').submit(function(e) {
+    e.preventDefault();
+    
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
@@ -90,7 +95,6 @@ jQuery(document).ready(function($) {
       }
     });
     if (ferror) return false;
-    else var str = $(this).serialize();
 
     var this_form = $(this);
     var action = $(this).attr('action');
@@ -104,23 +108,55 @@ jQuery(document).ready(function($) {
     this_form.find('.sent-message').slideUp();
     this_form.find('.error-message').slideUp();
     this_form.find('.loading').slideDown();
-    
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        if (msg == 'OK') {
-          this_form.find('.loading').slideUp();
-          this_form.find('.sent-message').slideDown();
-          this_form.find("input:not(input[type=submit]), textarea").val('');
-        } else {
-          this_form.find('.loading').slideUp();
-          this_form.find('.error-message').slideDown().html(msg);
-        }
-      }
-    });
-    return false;
+
+  var data = {
+    "clientId": "2",
+    "name": this_form.find('.form-control')[0].value,
+    "contactNo":this_form.find('.form-control')[1].value,
+    "email":this_form.find('.form-control')[2].value,
+    "message":this_form.find('.form-control')[3].value
+  }
+    mail(this_form, action, data);
+    return true;
   });
 
-});
+  function mail(this_form, action, data) {
+    $.ajax({
+      type: "POST",
+      url: "https://appseonit-mail.herokuapp.com/send",
+      data: data,
+      timeout: 40000
+    }).done( function(msg){
+      if (msg.trim() == 'OK') {
+        this_form.find('.loading').slideUp();
+        this_form.find('.sent-message').slideDown();
+        this_form.find("input:not(input[type=submit]), textarea").val('');
+      } else {
+        this_form.find('.loading').slideUp();
+        if(!msg) {
+          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
+        }
+        this_form.find('.error-message').slideDown().html(msg);
+      }
+    }).fail( function(data){
+      console.log(data);
+      var error_msg = "Form submission failed!<br>";
+      if(data.statusText || data.status) {
+        error_msg += 'Status:';
+        if(data.statusText) {
+          error_msg += ' ' + data.statusText;
+        }
+        if(data.status) {
+          error_msg += ' ' + data.status;
+        }
+        error_msg += '<br>';
+      }
+      if(data.responseText) {
+        error_msg += data.responseText;
+      }
+      this_form.find('.loading').slideUp();
+      this_form.find('.error-message').slideDown().html(error_msg);
+    });
+  }
+
+})(jQuery);
