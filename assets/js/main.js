8a54cdebbebb8c65132aafd4e24db17ceb179edc
MohammedHabeebkhan <mdhabeebkhan11446@gmail.com>
2021-03-08 20:37:32 +0530

    products and services carousel added in Trading

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,3 +213,21 @@ AOS.init({
       }
     }
   });
+
+  // Products carousel (uses the Owl Carousel library)
+  $(".products-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    }
+  });
